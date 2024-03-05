1a5969e1e2abb88f7bd3c96929e2675aefe6b734
makkar-nishant123 <makkar.nishant123@gmail.com>
2024-03-05 10:00:34 -0800

    Update DriverBase.java

--- src/test/java/com/lazerycode/selenium/DriverBase.java
+++ src/test/java/com/lazerycode/selenium/DriverBase.java
@@ -1,20 +1,13 @@
 package com.lazerycode.selenium;
 
-import com.lazerycode.selenium.config.DriverFactory;
 import com.lazerycode.selenium.listeners.ScreenshotListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
-import org.openqa.selenium.chrome.ChromeOptions;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Listeners(ScreenshotListener.class)
 public class DriverBase {
@@ -24,23 +17,10 @@ public class DriverBase {
 
     @BeforeSuite(alwaysRun = true)
     public static void instantiateDriverObject() {
-        
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") +  "\\chromedriver.exe");
-         // create chrome option
-        ChromeOptions options = new ChromeOptions();
-        // binary path
-        options.addArguments("--remote-allow-origins=*");
-        
-       // options.setBinary("//home//runner//work//GitHUBPOC//GitHUBPOC//chrome-linux64//chrome");
-      // options.setBinary(System.getProperty("user.dir") +  "\\chrome-win64\\chrome.exe");
-        
-         driver = new ChromeDriver(options);
-         
+         driver = new ChromeDriver();
     }
 
-
-
-    @AfterMethod(alwaysRun = true)
+    @AfterSuite(alwaysRun = true)
     public static void clearCookies() {
        driver.close();
     }
