fd2f4f792e79ce140d4ad0ca155f0df1083aa6c7
makkar-nishant123 <makkar.nishant123@gmail.com>
2024-03-05 09:58:18 -0800

    Update TelusPage.java

--- src/test/java/com/lazerycode/selenium/page_objects/TelusPage.java
+++ src/test/java/com/lazerycode/selenium/page_objects/TelusPage.java
@@ -1,22 +1,69 @@
 package com.lazerycode.selenium.page_objects;
 
-import com.lazerycode.selenium.DriverBase;
-import com.lazerycode.selenium.util.Query;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-import java.time.Duration;
-
-import static com.lazerycode.selenium.util.AssignDriver.initQueryObjects;
+import java.util.List;
 
 public class TelusPage {
 
-    private final Query acceptCookiesPopup = new Query().defaultLocator(By.xpath("//*[.='I agree']"));
-    private final Query searchBar = new Query().defaultLocator(By.name("q"));
-    private final Query googleSearch = new Query().defaultLocator(By.name("btnK"));
-    private final Query imFeelingLucky = new Query().defaultLocator(By.name("btnI"));
+    private WebDriver driver;
+
+    @FindBy(xpath = ".//*[@aria-label=\"Navigation Menu\"]")
+    private WebElement navigationMenu;
+
+    @FindBy(id = "close-cookies-notice-banner")
+    private WebElement banner;
+
+    @FindBy(xpath = "(.//*[@data-test='searchResultItem']/a)[8]")
+    private WebElement searchDropdownResult;
+
+    @FindBy(xpath = ".//*[contains(@class, \"styles__ListItem-sc\")]/a")
+    private WebElement searchStringResult;
+
+    @FindBy(xpath = "(.//*[@placeholder=\"Search TELUS.com\"])[2]")
+    private WebElement searchBar;
+
+    @FindBy(xpath = ".//*[contains(@class, \"styles__ListItem-sc\")]/a")
+    private List<WebElement> searchResults;
+
+    private String searchStringLocator = ".//*[@dir=\"auto\" and contains(text(),'xxxxxxxx')]";
+
+    public TelusPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    public void openNavigationMenu(){
+        navigationMenu.click();
+    }
+
+    public void acceptCookies(){
+        banner.click();
+    }
+
+    public void searchValue(String searchPhrase){
+        searchBar.sendKeys(searchPhrase);
+        try {
+            Thread.sleep(4000); //Waiting so search results appear in mean time.
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getSearchString(){
+       return searchDropdownResult.getText().replace(".","");
+           }
+
+    public void clickSearchString(){
+        searchDropdownResult.click();
+    }
+
+    public List<WebElement> getSearchResults(){
+        return searchResults;
+    }
 
 
 }
